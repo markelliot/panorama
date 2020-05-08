cd8877918ae4c9370f5d151a6ca743547b3420f6
Mark Elliot <mark.elliot@gmail.com>
2020-05-09 00:28:20 +0100

    add prod build

---
+++ webpack.prod.js
@@ -0,0 +1,10 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.config.js");
+const path = require("path");
+
+module.exports = merge(common, {
+    mode: "production",
+    output: {
+        path: path.join(__dirname, "build/production")
+    },
+});
